367a29c069f268a7be7c813cc1888193358457c0
Dylan Drechsel <drechseldylan@gmail.com>
2021-10-27 10:55:51 -0400

    server set up on port

--- backend/server.js
+++ backend/server.js
@@ -2,3 +2,23 @@ import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
 
+dotenv.config();
+const app = express()
+// const port = 4000
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+	cors({
+		// credentials: true,
+		origin: '*',
+	})
+);
+
+app.get('/', (req, res) => {
+	res.send('Welcome to SQL');
+});
+
+app.listen(process.env.PORT || port, () => {
+	console.log(`listening on ${process.env.PORT}`);
+});
