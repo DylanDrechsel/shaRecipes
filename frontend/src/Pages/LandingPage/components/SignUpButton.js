aba0ae70a55615b8367c92f47da66e263f3f2127
Dylan Drechsel <drechseldylan@gmail.com>
2021-12-02 13:56:15 -0500

    set up modal for SignUpButton.js

--- frontend/src/Pages/LandingPage/components/SignUpButton.js
+++ frontend/src/Pages/LandingPage/components/SignUpButton.js
@@ -1,16 +1,20 @@
-import React from 'react';
-import { Button } from 'react-bootstrap'
+import React, { useState } from 'react';
+import SignUpModal from './sign-up-components/SignUpModal';
 
 const SignUpButton = () => {
+    const [show, setShow] = useState(false);
+    const handleShow = () => setShow(true);
+
     return (
         <div className='lp-sub-main-div'>
-            <a href="#" class="animated-button1">
+            <a href="#" class="animated-button1" onClick={handleShow}>
                 <span></span>
                 <span></span>
                 <span></span>
                 <span></span>
             Sign Up
             </a>
+            <SignUpModal show={show} setShow={setShow}/>
         </div>
     );
 };
