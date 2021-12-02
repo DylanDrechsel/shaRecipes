d09ff4b37a4589ce33e1ec082aeea3e451fd04c2
Dylan Drechsel <drechseldylan@gmail.com>
2021-12-02 11:42:42 -0500

    Created PictureRow.js and imported it into LandingPage.js

--- frontend/src/Pages/LandingPage/LandingPage.js
+++ frontend/src/Pages/LandingPage/LandingPage.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './landing-page.css'
+import PictureRow from './components/PictureRow';
 
 const LandingPage = () => {
     return (
         <div className='lp-main-div'>
             <h1 className='lp-title'> <b> shaRecipes </b> </h1>
+            <PictureRow />
         </div>
     );
 };
