1d2b321ec859c796653bdca8a5b49b35900c2f8e
Dylan Drechsel <drechseldylan@gmail.com>
2021-12-02 12:08:01 -0500

    Completed SignInButton.js css

--- frontend/src/Pages/LandingPage/LandingPage.js
+++ frontend/src/Pages/LandingPage/LandingPage.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './landing-page.css'
 import PictureRow from './components/PictureRow';
 import SignUpButton from './components/SignUpButton';
+import SignInButton from './components/SignInButton';
 
 const LandingPage = () => {
     return (
         <div className='lp-main-div'>
             <h1 className='lp-title'> <b> shaRecipes </b> </h1>
             <PictureRow />
+            <SignInButton />
             <SignUpButton />
         </div>
     );
---
+++ frontend/src/Pages/LandingPage/components/SignInButton.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const SignInButton = () => {
+    return (
+        <div className='lp-sib-main-div'>
+            <a href="#" class="animated-button1">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+            Log In
+            </a>
+        </div>
+    );
+};
+
+export default SignInButton;
