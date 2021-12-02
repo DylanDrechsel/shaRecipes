d545f363c5c7a69c3a2c1acce5101119533888e2
Dylan Drechsel <drechseldylan@gmail.com>
2021-12-02 12:01:52 -0500

    Added SignUpButton.js and completed css editing" "

--- frontend/src/Pages/LandingPage/LandingPage.js
+++ frontend/src/Pages/LandingPage/LandingPage.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './landing-page.css'
 import PictureRow from './components/PictureRow';
+import SignUpButton from './components/SignUpButton';
 
 const LandingPage = () => {
     return (
         <div className='lp-main-div'>
             <h1 className='lp-title'> <b> shaRecipes </b> </h1>
             <PictureRow />
+            <SignUpButton />
         </div>
     );
 };
