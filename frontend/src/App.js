22bf3f69276871ee4cbe7ecbf22841952f945710
Dylan Drechsel <drechseldylan@gmail.com>
2021-12-08 10:51:12 -0500

    Server.js completed and is up and running'

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,36 +1,41 @@
 import './App.css';
-import { ApolloClient, InMemoryCache, ApolloProvider, ApolloLink, HttpLink } from '@apollo/client';
-import { setContext } from '@apollo/client/link/context';
-import { createHttpLink } from 'apollo-link-http';
+import { ApolloClient, InMemoryCache, ApolloProvider, HttpLink, from } from '@apollo/client';
+import { onError } from '@apollo/client/link/error';
 import { useRecoilState } from 'recoil';
 import { websiteState } from './recoil/atoms';
 import LandingPage from './Pages/LandingPage/LandingPage';
 
-// Create HttpLink for Apollo
-// const httpLink = HttpLink({
-// 	uri: 'http://localhost:4003/graphql',
-// });
+const errorLink = onError(({ graphqlErrors, networkErrors }) => {
+  if (graphqlErrors) {
+    graphqlErrors.map(({message, location, path}) => {
+      alert(`Graphql Error ${message}`)
+    })
+  }
+})
 
-// Initialize Apollo Client
-// const client = new ApolloClient({
-// 	// link: authLink.concat(httpLink),
-//   // link: httpLink,
-//   link: ApolloLink.from([httpLink]),
-// 	cache: new InMemoryCache(),
-// });
+const link = from([
+  errorLink, 
+  new HttpLink({
+    uri: 'http://localhost:4003/graphql'
+  })
+])
 
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: link
+})
 
 function App() {
   const [website, setWebite] = useRecoilState(websiteState)
 
   return (
-    // <ApolloProvider client={client}>
+    <ApolloProvider client={client}>
       <div className="App">
         <header className="App-header">
           <LandingPage />
         </header>
       </div>
-    // </ApolloProvider>
+    </ApolloProvider>
   );
 }
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -11,13 +11,89 @@ import {
   InMemoryCache,
   ApolloProvider,
   useQuery,
-  gql
+  gql,
+  ApolloLink,
+  HttpLink
 } from "@apollo/client";
-import { setContext } from '@apollo/client/link/context';
+import { onError } from '@apollo/client/link/error'
 import { createHttpLink } from 'apollo-link-http';
+import { setContext } from '@apollo/client/link/context';
 
 require('dotenv').config();
 
+
+
+
+
+
+
+
+
+
+
+
+// const client = new ApolloClient({
+//   cache: new InMemoryCache(),
+//   link: ApolloLink.from([
+//     new HttpLink({ uri: "http://localhost:4003"}),
+//     onError(({ graphQLErrors, networkError }) => {
+//       if (networkError) {
+//         console.log(`[Network error]: ${networkError}`);
+//       }
+ 
+//       if (graphQLErrors) {
+//         graphQLErrors.forEach(({ message, locations, path }) =>
+//           console.log(
+//             `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+//           )
+//         );
+//       }
+//     })
+//   ])
+// })
+
+// Create HttpLink for Apollo
+// const httpLink = createHttpLink({
+//   uri: 'http://localhost:4003'
+// })
+
+// Auth for token
+// const authLink = setContext((_, { headers }) => {
+// 	return {
+// 		headers: {
+// 			...headers,
+// 			// authorization: token ? `${token}` : ''
+// 		}
+// 	}
+// })
+
+// Initialize Apollo Client
+// const client = new ApolloClient({
+// 	link: authLink.concat(httpLink),
+// 	cache: new InMemoryCache(),
+// });
+
+// const client = new ApolloClient({
+//   uri: 'http://localhost:4003/graphql',
+//   cache: new InMemoryCache()
+// });
+
+// const error = apolloError // from async try/catch, onError method, or a promise .catch
+// console.log(JSON.stringify(error, null, 2));
+
+// client
+//   .query({
+//     query: gql`
+//       query GetRates {
+//         rates(currency: "USD") {
+//           currency
+//         }
+//       }
+//     `
+//   })
+//   .then(result => console.log(result));
+
+// http://172.19.0.3:4003
 // Create HttpLink for Apollo
 // const httpLink = createHttpLink({
 // 	uri: 'http://localhost:4003/graphql',
